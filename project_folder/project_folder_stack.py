93481f2933bc44f86a8d0cd35bc19b294388580a
root <keaton.ding.1997@gmail.com>
2024-08-14 22:53:42 -0400

    ready to run

--- project_folder/project_folder_stack.py
+++ project_folder/project_folder_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
-    # Duration,
+    aws_lambda as _lambda,
     Stack,
+    CfnOutput
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -9,7 +10,28 @@ class ProjectFolderStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        my_function = _lambda.Function(
+          self, "HelloWorldFunction", 
+          runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+          handler = "index.handler",
+          code = _lambda.Code.from_inline(
+            """
+            exports.handler = async function(event) {
+              return {
+                statusCode: 200,
+                body: JSON.stringify('Hello World!'),
+              };
+            };
+            """
+          ),
+        )
 
+        my_function_url = my_function.add_function_url(
+            auth_type = _lambda.FunctionUrlAuthType.NONE,
+        )
+
+        # Define a CloudFormation output for your URL
+        CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
         # The code that defines your stack goes here
 
         # example resource
